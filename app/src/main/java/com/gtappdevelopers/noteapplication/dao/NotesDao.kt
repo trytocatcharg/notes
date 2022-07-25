acd68b294b6b4ac70402bfe64582eb16594fcc88
HDD_REPO <trytocatch.arg@gmail.com>
2022-07-25 17:19:50 +0200

    init commit

---
+++ app/src/main/java/com/gtappdevelopers/noteapplication/dao/NotesDao.kt
@@ -0,0 +1,23 @@
+package com.gtappdevelopers.noteapplication.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.gtappdevelopers.noteapplication.entities.Note
+
+@Dao
+interface NotesDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note : Note)
+
+    @Update
+    suspend fun update(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("Select * from notesTable order by id ASC")
+    fun getAllNotes(): LiveData<List<Note>>
+
+
+}
---
+++ app/src/main/java/com/gtappdevelopers/noteapplication/viewmodels/NoteViewModal.kt
@@ -0,0 +1,34 @@
+package com.gtappdevelopers.noteapplication.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.gtappdevelopers.noteapplication.DB.NoteDatabase
+import com.gtappdevelopers.noteapplication.entities.Note
+import com.gtappdevelopers.noteapplication.repositories.NoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModal(application: Application) : AndroidViewModel(application) {
+    val allNotes: LiveData<List<Note>>
+    val repository: NoteRepository
+
+    init {
+        val dao = NoteDatabase.getDatabase(application).getNotesDao()
+        repository = NoteRepository(dao)
+        allNotes = repository.allNotes
+    }
+
+    fun deleteNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(note)
+    }
+
+    fun updateNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.update(note)
+    }
+
+    fun addNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(note)
+    }
+}
